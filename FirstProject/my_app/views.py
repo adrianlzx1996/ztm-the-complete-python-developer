2bf816066c77de83b759d2bfe25953ff63c1a4f9
Adrian Leong <leongadrian36@gmail.com>
2025-04-21 23:31:29 +0800

    Add 'add' view and URL route for addition functionality
    Added a new view `add` to handle addition of two integers and display the result. Updated `urls.py` to include a new route for this functionality. This enhances the application with a basic arithmetic feature.

--- FirstProject/my_app/views.py
+++ FirstProject/my_app/views.py
@@ -10,3 +10,6 @@ def about(request):
 
 def hello(request, first_name):
     return HttpResponse(f"Hello {first_name}")
+
+def add(request, num1, num2):
+    return HttpResponse(f"The sum of {num1} + {num2} = {int(num1) + int(num2)}")
