e60d0ba5019f2be58d501715f08f71f49d722bb9
Adrian Leong <leongadrian36@gmail.com>
2025-04-22 08:18:27 +0800

    Update index view to pass movie context to template
    The `index` view now includes a context dictionary with a 'movie' key, passing the value 'gladiator' to the template. This change is reflected in the `index.html` template, where the `movie` variable is displayed dynamically.

--- FavoriteMovie/movies/views.py
+++ FavoriteMovie/movies/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html', {})
+    return render(request, 'movies/index.html', {'movie': 'gladiator'})
+
 
 def about(request):
     return render(request, 'movies/about.html', {})
