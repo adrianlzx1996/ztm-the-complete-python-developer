2ed03555e1a37fa92b90472a7acb7cf469e4cfd7
Adrian Leong <leongadrian36@gmail.com>
2025-04-22 08:27:26 +0800

    Update movie list rendering in index view and template
    Revised the `index` view to pass a list of movies instead of a single movie. Updated the template to display the list dynamically, with a fallback message if no movies are provided.

--- FavoriteMovie/movies/views.py
+++ FavoriteMovie/movies/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'movies/index.html', {'movie': 'gladiator'})
+    context = {'movies': ['gladiator', 'top gun', 'casino royale']}
+    return render(request, 'movies/index.html', context)
 
 
 def about(request):
