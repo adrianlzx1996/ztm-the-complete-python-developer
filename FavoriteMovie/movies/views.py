eaf0f71f84d1a6034f19fe7aae0daad96a23c6b0
Adrian Leong <leongadrian36@gmail.com>
2025-04-22 08:11:23 +0800

    Add an "About" page to the movies app
    Created a new "About" view and template to display information about movie preferences. Updated the URLs configuration to include a route for the "About" page.

--- FavoriteMovie/movies/views.py
+++ FavoriteMovie/movies/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'movies/index.html', {})
+
+def about(request):
+    return render(request, 'movies/about.html', {})
